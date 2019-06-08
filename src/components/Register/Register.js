30397061cbe7e5064eb74b93a02a7a82b9245bbb
Kevin Snijders <kevin_snijders@live.nl>
2019-06-09 00:08:07 +0200

    Replaced css for Sign up and Register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,36 +1,87 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
+
+const BorderRadius = css`
+	border-radius: .5rem;
+`;
+
+const FormElement = css`
+	width: 100%;
+	font-size: 1.6rem;
+	padding: 1.6rem;
+	margin-bottom: 1.8rem;
+	${BorderRadius}
+`;
+
+const Form = styled.div`
+	width: 25vw;
+	border-color: rgba( 0, 0, 0, .1 );
+	box-shadow: 0.4rem 0.4rem 0.8rem 0 rgba( 0, 0, 0, .2 );
+	margin: 0 auto;
+	${BorderRadius}
+`;
+
+const FormBackground = styled.div`
+	background-color: ${props => props.primary ? "rgba(238, 238, 238, 0.8)" : "rgba(255, 255, 255, 0.9)"}
+`;
+
+const Title = styled.h4`
+	color: #333;
+	font-size: 1.8rem;
+	text-transform: uppercase;
+	font-weight: 700;
+	text-align: center;
+	margin: 2.4rem 0;
+`;
+
+const Wrapper = styled.div`
+	padding: 1.6rem 2.4rem;
+`;
+
+
+const InputField = styled.input`
+	background-color: #FFF;
+	border: 2px solid #dadada;
+	${FormElement}
+`;
+
+const Button = styled.input`
+    width: 100%;
+    background-color: #ca3435;
+    color: #FFF;
+    text-transform: uppercase;
+    border: 2px solid #ca3435;
+    transition: transform .15s ease-out;
+    cursor: pointer;
+    ${FormElement}
+    
+    &:active {
+        transform: translateY(2px);
+    }
+`;
 
 const Register = ({onRouteChange}) => {
-	return (<div className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
-		<section className="pa4 black-80">
-			<form className="measure">
-				<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-					<legend className="f1 fw6 ph0 mh0">Register</legend>
-					<div className="mt3">
-						<label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-						<input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-						       type="text" name="name" id="name"/>
-					</div>
-					<div className="mt3">
-						<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-						<input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-						       type="email" name="email-address" id="email-address"/>
-					</div>
-					<div className="mv3">
-						<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-						<input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-						       type="password" name="password" id="password"/>
-					</div>
-				</fieldset>
-				<div className="">
-					<input onClick={() => onRouteChange('home')}
-					       className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-					       type="submit" value="Register"/>
-				</div>
-			</form>
-		</section>
-	</div>)
+	return (
+		<Form>
+			<FormBackground primary>
+				<Wrapper top>
+					<Title>Register</Title>
+					<InputField type="text" name="name" id="name"
+					            placeholder="Name">
+					</InputField>
+					<InputField type="email" name="email-address" id="email-address"
+					            placeholder="Email Address">
+					</InputField>
+					<InputField type="password" name="password" id="password"
+					            placeholder="Password">
+					</InputField>
+					<Button onClick={() => onRouteChange('home')}
+					        type="submit" value="Register">
+					</Button>
+				</Wrapper>
+			</FormBackground>
+		</Form>
+	)
 };
-
 
 export default Register;
