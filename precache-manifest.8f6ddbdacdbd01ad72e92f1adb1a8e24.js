cb2a596246eb73a500e592abeaf8bd3b90140194
Kevin Snijders <kevin_snijders@live.nl>
2019-08-06 10:24:09 +0200

    Updates

---
+++ precache-manifest.8f6ddbdacdbd01ad72e92f1adb1a8e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395a044a8ca38e74df1ceb5f92f46937",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "67b2802ec93d8bc3f354",
+    "url": "/facerecognition/static/css/2.5c4d1152.chunk.css"
+  },
+  {
+    "revision": "67b2802ec93d8bc3f354",
+    "url": "/facerecognition/static/js/2.fdaf4c64.chunk.js"
+  },
+  {
+    "revision": "7e9689c8f5c82d87ea5b",
+    "url": "/facerecognition/static/js/main.ef4455fe.chunk.js"
+  },
+  {
+    "revision": "9a6a6a0c42958dd8e526",
+    "url": "/facerecognition/static/js/runtime~main.7f9ce1ea.js"
+  }
+]);
